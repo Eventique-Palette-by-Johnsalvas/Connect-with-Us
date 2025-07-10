05eafafd8b6e10b34b34b24e53ea733e77206a0b
Eventique Palette <eventiquepalette@gmail.com>
2025-07-10 05:58:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("chatButton").addEventListener("click", () => {
+    const popup = document.getElementById("chatPopup");
+    popup.style.display = popup.style.display === "block" ? "none" : "block";
+});
